6d7682dff1634b20d456d76338d28404239cf752
Leonardo Rocha <lrochadev@gmail.com>
2026-04-26 01:08:43 -0300

    feat(sns): Add configuration for SNS message publisher

--- internal/config/config.go
+++ internal/config/config.go
@@ -21,6 +21,13 @@ type Config struct {
 	VisibilityTimeout int32
 	WaitTimeSeconds   int32
 	ShutdownTimeout   time.Duration
+
+	SNSTopicARN    string
+	SNSEndpoint    string
+	SNSBatchSize   int
+	SNSLinger      time.Duration
+	SNSPublishers  int
+	SNSInputBuffer int
 }
 
 func Load() (Config, error) {
@@ -37,11 +44,32 @@ func Load() (Config, error) {
 		return Config{}, errors.New("AWS_REGION is required")
 	}
 
+	topicARN := os.Getenv("SNS_TOPIC_ARN")
+	if topicARN == "" {
+		return Config{}, errors.New("SNS_TOPIC_ARN is required")
+	}
+
+	sqsEndpoint := os.Getenv("SQS_ENDPOINT")
+	snsEndpoint := os.Getenv("SNS_ENDPOINT")
+	if snsEndpoint == "" {
+		snsEndpoint = sqsEndpoint
+	}
+
+	workers := getEnvInt("WORKERS", 10)
+
+	batchSize := getEnvInt("SNS_BATCH_SIZE", 10)
+	if batchSize < 1 {
+		batchSize = 1
+	}
+	if batchSize > 10 {
+		batchSize = 10
+	}
+
 	return Config{
 		QueueURL:          queueURL,
 		Region:            region,
-		Endpoint:          os.Getenv("SQS_ENDPOINT"),
-		Workers:           getEnvInt("WORKERS", 10),
+		Endpoint:          sqsEndpoint,
+		Workers:           workers,
 		Pollers:           getEnvInt("POLLERS", 3),
 		MaxAttempts:       getEnvInt("MAX_ATTEMPTS", 3),
 		BaseBackoff:       getEnvDuration("BASE_BACKOFF", 2*time.Second),
@@ -49,6 +77,13 @@ func Load() (Config, error) {
 		VisibilityTimeout: int32(getEnvInt("VISIBILITY_TIMEOUT_SECS", 30)),
 		WaitTimeSeconds:   int32(getEnvInt("WAIT_TIME_SECS", 20)),
 		ShutdownTimeout:   getEnvDuration("SHUTDOWN_TIMEOUT", 30*time.Second),
+
+		SNSTopicARN:    topicARN,
+		SNSEndpoint:    snsEndpoint,
+		SNSBatchSize:   batchSize,
+		SNSLinger:      getEnvDuration("SNS_LINGER", 150*time.Millisecond),
+		SNSPublishers:  getEnvInt("SNS_PUBLISHERS", 8),
+		SNSInputBuffer: getEnvInt("SNS_INPUT_BUFFER", workers*4),
 	}, nil
 }
 
